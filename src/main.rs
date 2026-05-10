48d3f8f42175cf7fceb4243ba27ee9750e991743
VeryBaaad <verybaaad@outlook.com>
2026-05-10 19:48:16 +0800

    fix: make version command await

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ async fn commands_handler(
             handle_rank(bot, msg.chat.id, None, Some(msg.id), pool, page).await?;
         },
         Command::Version => {
-            let _ = bot.send_message(msg.chat.id, format!("{} v{}\nCommit {}\nBuilt at {}\nTarget {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"), env!("GIT_HASH"), env!("BUILD_TIME"), env!("BUILD_TARGET")));
+            bot.send_message(msg.chat.id, format!("{} v{}\nCommit {}\nBuilt at {}\nTarget {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"), env!("GIT_HASH"), env!("BUILD_TIME"), env!("BUILD_TARGET"))).await?;
         }
     }
     Ok(())
