62b4b8a7498d889d09270a8a73d06a901334be91
VeryBaaad <verybaaad@outlook.com>
2026-05-12 23:24:29 +0800

    feat(version): show version code

--- build.rs
+++ build.rs
@@ -2,14 +2,21 @@ use std::process::Command;
 use chrono::Local;
 
 fn main() {
-    let output = Command::new("git")
+    let shortcommit = Command::new("git")
         .args(["rev-parse", "--short", "HEAD"])
         .output()
         .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap().trim().to_string();
+    let count = Command::new("git")
+        .args(["rev-list", "--count", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(shortcommit.stdout).unwrap().trim().to_string();
+    let git_count_str = String::from_utf8(count.stdout).unwrap().trim().to_string();
+    let git_count: u64 = git_count_str.parse().unwrap_or(0);
     let build_time = Local::now().format("%Y-%m-%dT%H:%M:%S%.3f");
     let target = std::env::var("TARGET").unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    println!("cargo:rustc-env=VER_CODE={}", git_count + 500);
     println!("cargo:rustc-env=BUILD_TIME={}", build_time);
     println!("cargo:rustc-env=BUILD_TARGET={}", target);
     println!("cargo:rerun-if-changed=.git/HEAD");
