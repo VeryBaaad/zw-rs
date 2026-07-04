2fe1317abca884ede46ae1a10ef37a22a876dd7a
VeryBaaad <verybaaad@outlook.com>
2026-07-05 01:44:27 +0800

    fix

--- src/services/zw.rs
+++ src/services/zw.rs
@@ -76,6 +76,9 @@ pub async fn handle_zw(
             .await;
         return Ok(());
     };
+    if target_user_id == initiator_id {
+        return Ok(());
+    }
     let initiator_mention = format_user_mention(
         initiator_id,
         initiator_first_name,
@@ -551,6 +554,9 @@ pub async fn process_zw_help_for_user(
         target.last_name,
         target.username,
     );
+    if target.user_id == initiator.user_id {
+        return Ok(("405 Method Not Allowed".to_string(), false, None));
+    }
     log(
         Level::Debug,
         "process_zw_help_for_user",
